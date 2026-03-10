01f102b34a7769810194dfc9a43ed5f44865de91
Morgan Campbell <20241069@mywhitecliffe.com>
2026-03-10 13:44:33 +1300

    week 3 additions

---
+++ Recipe Viewer/recipeViewer/recipes/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Recipe
+
+# Create your views here.
+class RecipeListView(ListView):
+    model = Recipe
+    template_name = "recipes/home.html"
